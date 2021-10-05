412a2baa7b3db89a6fb70a923ac58aa8711a8143
Antonio Berardi <adberardi@yahoo.com>
2021-10-05 14:15:25 -0400

    Clase 23: Service Workers

--- assets/index.js
+++ assets/index.js
@@ -14,3 +14,9 @@ const player = new MediaPlayer({
 });
 button.onclick = () => player.play();
 btnMuted.onclick = () => player.mute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
+}
---
+++ sw.js
@@ -0,0 +1,45 @@
+// self es el this en los service workers (sw)
+self.addEventListener('install',event => {
+    //precache
+    event.waitUntil(precache());
+});
+
+self.addEventListener('fetch',event => {
+    const request = event.request;
+    //get 
+    if (request !== "GET") {
+        return;
+    }
+
+    // Buscar en cache
+    event.respondwith(cachedResponse(request));
+
+    //Actualizar el cache
+    event.waitUntil(updateCache())
+})
+
+async function precache() {
+    const cache = await caches.open("v1");
+    return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/index.js',
+        '/assets/MediaPlayer.js',
+        '/assets/plugins/AutoPlay.js',
+        '/assets/plugins/AutoPause.js',
+        '/assets/index.css',
+        '/assets/BigBuckBunny.mp4',
+    ]);
+}
+
+async function cachedResponse(request) {
+    const cache = await caches.open("v1");
+    const response = await cache.match(request);
+    return response || fetch(request);
+}
+
+async function updateCache(request) {
+    const cache = await caches.open("v1");
+    const response = await fetch(request);
+    return cache.put(request,response);
+}
