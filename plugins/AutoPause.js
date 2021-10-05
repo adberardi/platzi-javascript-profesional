78caa031c502583555e3b4cd0407b0e5725ea92b
Antonio Berardi <adberardi@yahoo.com>
2021-10-05 13:47:07 -0400

    Clase 22: VisibilityChange

--- plugins/AutoPause.js
+++ plugins/AutoPause.js
@@ -2,15 +2,19 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this)
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
 
   run(player) {
     this.player = player;
+/*   Otra forma:
     const config = { threshold: 0.25 };
+    const observer = new IntersectionObserver(this.handleIntersection, config); */
     const observer = new IntersectionObserver(this.handleIntersection, {
       threshold: this.threshold,
     });
     observer.observe(this.player.media);
+    document.addEventListener("visibilitychange", this.handleVisibilityChange)
   }
 
   handleIntersection(entries) {
@@ -22,6 +26,15 @@ class AutoPause {
     } else {
       this.player.pause();
     }
+  }
+
+  handleVisibilityChange(){
+      const isVisible = document.visibilityState === "visible";
+      if (isVisible) {
+          this.player.play();
+      } else {
+          this.player.pause();
+      }
   }
 }
 
