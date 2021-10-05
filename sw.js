77d41f36117ffe3baf3990bbbc35f320e416345f
Antonio Berardi <adberardi@yahoo.com>
2021-10-05 14:36:59 -0400

    Clase 23: Service Workers - Solventando Errores

--- sw.js
+++ sw.js
@@ -21,14 +21,14 @@ self.addEventListener('fetch',event => {
 async function precache() {
     const cache = await caches.open("v1");
     return cache.addAll([
-        '/',
-        '/index.html',
-        '/assets/index.js',
-        '/assets/MediaPlayer.js',
-        '/assets/plugins/AutoPlay.js',
-        '/assets/plugins/AutoPause.js',
-        '/assets/index.css',
-        '/assets/BigBuckBunny.mp4',
+        './',
+        './index.html',
+        './assets/index.js',
+        './assets/MediaPlayer.js',
+        './plugins/AutoPlay.js',
+        './plugins/AutoPause.js',
+        './assets/index.css',
+        './assets/BigBuckBunny.mp4',
     ]);
 }
 
